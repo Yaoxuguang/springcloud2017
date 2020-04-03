9ff10d28badaa154b126d4e4a2300018d741473a
yaoxuguang <yaoxuguang@ele-cloud.com>
2020-04-03 16:41:53 +0800

    给不同环境的配置文件添加内容

---
+++ spring-cloud-config-client-demo/src/test/java/com/yao/springcloudconfigclientdemo/SpringCloudConfigClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yao.springcloudconfigclientdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigClientDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
