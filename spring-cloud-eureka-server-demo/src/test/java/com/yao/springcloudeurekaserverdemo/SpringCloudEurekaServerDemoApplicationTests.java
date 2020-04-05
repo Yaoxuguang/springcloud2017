4dd623bd77f64cbd502d7dbe8289a7da8bfea6fe
yaoxuguang <yaoxuguang@ele-cloud.com>
2020-04-05 23:45:28 +0800

    spring cloud eureka 服务端代码提交

---
+++ spring-cloud-eureka-server-demo/src/test/java/com/yao/springcloudeurekaserverdemo/SpringCloudEurekaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yao.springcloudeurekaserverdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
